e4fada8a6ca3090a2e257e792836c1c4348508e8
akulsharma1 <akulsharma02@gmail.com>
2021-06-09 22:44:20 -0700

    Changed some stuff in webhook

--- monitor/webhooks.go
+++ monitor/webhooks.go
@@ -17,24 +17,24 @@ func (t *Scraper) SendNewProdWebhook() {
 	for i := range VariantMaps {
 		e.AddField("Size "+SizeMaps[i], "[ATC]"+ fmt.Sprintf("(%vcart/add?id=%v)", t.BaseURL, VariantMaps[i]), true)
 	}
-	e.SetFooter("R2RFNF • " + currentTime.Format("[01/02 15:04:05]")+ " • splash#0003", "https://media.discordapp.net/attachments/783026351531491361/852038253863632906/R2R.png?width=559&height=559")
+	e.SetFooter("R2R • " + currentTime.Format("[01/02 15:04:05]")+ " • splash#0003", "https://media.discordapp.net/attachments/783026351531491361/852038253863632906/R2R.png?width=559&height=559")
 	e.SendToWebhook(webhook)
 }
 
 func (t *Scraper) SendProductsRemovedWebhook() {
 	currentTime := time.Now()
-	e := godiscord.NewEmbed("", fmt.Sprintf("Products pulled on %v", t.BaseURL), "")
+	e := godiscord.NewEmbed("", fmt.Sprintf("All Products pulled on %v", t.BaseURL), "")
 	e.SetColor("#76a9db")
 	e.SetAuthor(t.BaseURL, "", "")
-	e.SetFooter("R2RFNF • " + currentTime.Format("[01/02 15:04:05]")+ " • splash#0003", "https://media.discordapp.net/attachments/783026351531491361/852038253863632906/R2R.png?width=559&height=559")
+	e.SetFooter("R2R • " + currentTime.Format("[01/02 15:04:05]")+ " • splash#0003", "https://media.discordapp.net/attachments/783026351531491361/852038253863632906/R2R.png?width=559&height=559")
 	e.SendToWebhook(webhook)
 }
 
 func (t *Scraper) SendPwPageUpWebhook() {
 	currentTime := time.Now()
-	e := godiscord.NewEmbed("", fmt.Sprintf("Products pulled on %v", t.BaseURL), "")
+	e := godiscord.NewEmbed("", fmt.Sprintf("Password page up on %v", t.BaseURL), "")
 	e.SetColor("#76a9db")
 	e.SetAuthor(t.BaseURL, "", "")
-	e.SetFooter("R2RFNF • " + currentTime.Format("[01/02 15:04:05]")+ " • splash#0003", "https://media.discordapp.net/attachments/783026351531491361/852038253863632906/R2R.png?width=559&height=559")
+	e.SetFooter("R2R • " + currentTime.Format("[01/02 15:04:05]")+ " • splash#0003", "https://media.discordapp.net/attachments/783026351531491361/852038253863632906/R2R.png?width=559&height=559")
 	e.SendToWebhook(webhook)
 }
