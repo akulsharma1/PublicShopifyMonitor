3aad1583cf7895bbc31a8ae9c345dc63c72fca7d
akulsharma1 <akulsharma02@gmail.com>
2021-06-10 20:21:33 -0700

    Removed webhook from webhooks.go

--- monitor/webhooks.go
+++ monitor/webhooks.go
@@ -5,20 +5,18 @@ import (
 	"github.com/aiomonitors/godiscord"
 	"fmt"
 )
-var webhook = "https://discord.com/api/webhooks/848075809594802197/HqGi437WTNC1a2ItlKbDZrJb0RJzA8_XryVWwtqMlU988xw_2ajt-jZCLdK_jZzb6uJ7"
-
 func (t *Scraper) SendNewProdWebhook() {
 	currentTime := time.Now()
 	e := godiscord.NewEmbed(t.ProductTitle, "", t.BaseURL+"products/"+t.Handle)
 	e.SetAuthor(t.BaseURL, "", "")
-	e.SetColor("#00FF00")
+	e.SetColor("#00f7f7")
 	e.SetThumbnail(t.ImageURL)
 	e.AddField("Price", t.ProductPrice, false)
 	for i := range VariantMaps {
 		e.AddField("Size "+SizeMaps[i], "[ATC]"+ fmt.Sprintf("(%vcart/add?id=%v)", t.BaseURL, VariantMaps[i]), true)
 	}
 	e.SetFooter("R2R • " + currentTime.Format("[01/02 15:04:05]")+ " • splash#0003", "https://media.discordapp.net/attachments/783026351531491361/852038253863632906/R2R.png?width=559&height=559")
-	e.SendToWebhook(webhook)
+	e.SendToWebhook(t.Webhook)
 }
 
 func (t *Scraper) SendProductsRemovedWebhook() {
@@ -27,7 +25,7 @@ func (t *Scraper) SendProductsRemovedWebhook() {
 	e.SetColor("#76a9db")
 	e.SetAuthor(t.BaseURL, "", "")
 	e.SetFooter("R2R • " + currentTime.Format("[01/02 15:04:05]")+ " • splash#0003", "https://media.discordapp.net/attachments/783026351531491361/852038253863632906/R2R.png?width=559&height=559")
-	e.SendToWebhook(webhook)
+	e.SendToWebhook(t.Webhook)
 }
 
 func (t *Scraper) SendPwPageUpWebhook() {
@@ -36,5 +34,5 @@ func (t *Scraper) SendPwPageUpWebhook() {
 	e.SetColor("#76a9db")
 	e.SetAuthor(t.BaseURL, "", "")
 	e.SetFooter("R2R • " + currentTime.Format("[01/02 15:04:05]")+ " • splash#0003", "https://media.discordapp.net/attachments/783026351531491361/852038253863632906/R2R.png?width=559&height=559")
-	e.SendToWebhook(webhook)
+	e.SendToWebhook(t.Webhook)
 }
