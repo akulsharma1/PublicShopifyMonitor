6f1eeb9d036eb299ddb75e3cdde1de0b515caa25
akulsharma1 <akulsharma02@gmail.com>
2021-06-11 14:01:19 -0700

    Switched webhook to r2r webhook

--- main.go
+++ main.go
@@ -8,10 +8,11 @@ import (
 	//"io/ioutil"
 )
 
-var webhook = "https://discord.com/api/webhooks/843003288487067710/rFw0ZGER5_s1QCOD_1h-496-DGpk7U1qATlkOD6rTajYAWhUCcv1BL81Naj-NVai8pQ6"
+//var personalwebhook = "https://discord.com/api/webhooks/843003288487067710/rFw0ZGER5_s1QCOD_1h-496-DGpk7U1qATlkOD6rTajYAWhUCcv1BL81Naj-NVai8pQ6"
+var r2rwebhook = "https://canary.discord.com/api/webhooks/852726307346841610/zhT7akZ2VnDLdrXuFFl8BdCDf0TanqRk0TSPskTm8fn-IhxvAHDsJI2cITGr2pJKLBWs"
  
 func main() {
-	a := monitor.Scraper{BaseURL: "https://splashstore2.myshopify.com/", Webhook: webhook,}
+	a := monitor.Scraper{BaseURL: "https://splashstore2.myshopify.com/", Webhook: r2rwebhook,}
 
 	a.Monitor()
 	//postlocalhost()
