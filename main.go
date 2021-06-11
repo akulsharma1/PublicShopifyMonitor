538316735493edd9c5359c7320dfbe8f714f5963
akulsharma1 <akulsharma02@gmail.com>
2021-06-11 14:03:10 -0700

    Changed webhook

--- main.go
+++ main.go
@@ -9,10 +9,11 @@ import (
 )
 
 //var personalwebhook = "https://discord.com/api/webhooks/843003288487067710/rFw0ZGER5_s1QCOD_1h-496-DGpk7U1qATlkOD6rTajYAWhUCcv1BL81Naj-NVai8pQ6"
-var r2rwebhook = "https://canary.discord.com/api/webhooks/852726307346841610/zhT7akZ2VnDLdrXuFFl8BdCDf0TanqRk0TSPskTm8fn-IhxvAHDsJI2cITGr2pJKLBWs"
+//var r2reewebhook = "https://canary.discord.com/api/webhooks/852726307346841610/zhT7akZ2VnDLdrXuFFl8BdCDf0TanqRk0TSPskTm8fn-IhxvAHDsJI2cITGr2pJKLBWs"
+var r2rrichiewebhook = "https://discord.com/api/webhooks/853016234442489947/rIe1nUggovKQn81-uodKoJ7cXWjl3XBa8Nc5TcbLbb6vK8dmVjYr5R-yvrtmsv-YJ0LY"
  
 func main() {
-	a := monitor.Scraper{BaseURL: "https://splashstore2.myshopify.com/", Webhook: r2rwebhook,}
+	a := monitor.Scraper{BaseURL: "https://splashstore2.myshopify.com/", Webhook: r2rrichiewebhook,}
 
 	a.Monitor()
 	//postlocalhost()
