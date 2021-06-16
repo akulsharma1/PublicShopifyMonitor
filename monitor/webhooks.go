ec8452479e9a91f1ee11315e3573e5fdda5ab1ba
akulsharma1 <akulsharma02@gmail.com>
2021-06-15 22:02:30 -0700

    Added quicktasks

--- monitor/webhooks.go
+++ monitor/webhooks.go
@@ -15,6 +15,7 @@ func (t *Scraper) SendNewProdWebhook() {
 	for i := range VariantMaps {
 		e.AddField("Size "+SizeMaps[i], "[ATC]"+ fmt.Sprintf("(%vcart/add?id=%v)", t.BaseURL, VariantMaps[i]), true)
 	}
+	e.AddField("Quicktasks", fmt.Sprintf("[Wrath](http://localhost:32441/qt?input=%vproducts/%v) | ", t.BaseURL, t.Handle) + fmt.Sprintf("[Balko](http://localhost:6776/?url=%vproducts/%v) | ", t.BaseURL, t.Handle) + fmt.Sprintf("[Nebula](http://localhost:7392/quicktask?url=%vproducts/%v) | ", t.BaseURL, t.Handle) + fmt.Sprintf("[Cyber](https://cybersole.io/dashboard/tasks?quicktask=%vproducts/%v) | ", t.BaseURL, t.Handle) + fmt.Sprintf("[Prism](https://dashboard.prismaio.com?url=%vproducts/%v) | ", t.BaseURL, t.Handle) + fmt.Sprintf("[Sole](https://www.soleaio.dev/quick_tasks/create?taskGroup=Shopify&product=%vproducts/%v)", t.BaseURL, t.Handle), false)
 	e.SetFooter("R2R • " + currentTime.Format("[01/02 15:04:05]")+ " • splash#0003", "https://media.discordapp.net/attachments/783026351531491361/852038253863632906/R2R.png?width=559&height=559")
 	e.SendToWebhook(t.Webhook)
 	//fmt.Println(e.Content)
@@ -33,6 +34,14 @@ func (t *Scraper) SendProductsRemovedWebhook() {
 func (t *Scraper) SendPwPageUpWebhook() {
 	currentTime := time.Now()
 	e := godiscord.NewEmbed("", fmt.Sprintf("Password page up on %v", t.BaseURL), "")
+	e.SetColor("#76a9db")
+	e.SetAuthor(t.BaseURL, "", "")
+	e.SetFooter("R2R • " + currentTime.Format("[01/02 15:04:05]")+ " • splash#0003", "https://media.discordapp.net/attachments/783026351531491361/852038253863632906/R2R.png?width=559&height=559")
+	e.SendToWebhook(t.Webhook)
+}
+func (t *Scraper) SendPwPageDownWebhook() {
+	currentTime := time.Now()
+	e := godiscord.NewEmbed("", fmt.Sprintf("Password page down on %v", t.BaseURL), "")
 	e.SetColor("#76a9db")
 	e.SetAuthor(t.BaseURL, "", "")
 	e.SetFooter("R2R • " + currentTime.Format("[01/02 15:04:05]")+ " • splash#0003", "https://media.discordapp.net/attachments/783026351531491361/852038253863632906/R2R.png?width=559&height=559")
