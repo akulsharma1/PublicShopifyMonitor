92978f890c69dc8e200243856c5903a4b33d164a
akulsharma1 <akulsharma02@gmail.com>
2021-06-17 13:50:38 -0700

    Added some stuff

--- main.go
+++ main.go
@@ -1,18 +1,16 @@
 package main
 import (
-	//"strings"
 	"fmt"
-	//"net/url"
 	"splashshopifymonitor/monitor"
-	//"net/http"
-	//"io/ioutil"
-	//"os"
+	"splashshopifymonitor/taskengine"
 )
 
-var personalwebhook = "https://discord.com/api/webhooks/843003288487067710/rFw0ZGER5_s1QCOD_1h-496-DGpk7U1qATlkOD6rTajYAWhUCcv1BL81Naj-NVai8pQ6"
-//var r2reewebhook = "https://canary.discord.com/api/webhooks/852726307346841610/zhT7akZ2VnDLdrXuFFl8BdCDf0TanqRk0TSPskTm8fn-IhxvAHDsJI2cITGr2pJKLBWs"
+//var personalwebhook = "https://discord.com/api/webhooks/843003288487067710/rFw0ZGER5_s1QCOD_1h-496-DGpk7U1qATlkOD6rTajYAWhUCcv1BL81Naj-NVai8pQ6"
+//var r2reewebhook = "https://discord.com/api/webhooks/852726307346841610/zhT7akZ2VnDLdrXuFFl8BdCDf0TanqRk0TSPskTm8fn-IhxvAHDsJI2cITGr2pJKLBWs"
 //var r2rrichiewebhook = "https://discord.com/api/webhooks/853016234442489947/rIe1nUggovKQn81-uodKoJ7cXWjl3XBa8Nc5TcbLbb6vK8dmVjYr5R-yvrtmsv-YJ0LY"
 //var r2rbodegawebhook = "https://discord.com/api/webhooks/854535961601507338/kKACrV-FelmxK8VzHXjgf7Hb3dfeANXAHmXZ-8XT3wE4i4P0u72NkJH5OBpNGEA4sR-u"
+var r2rlfwebhook = "https://discord.com/api/webhooks/853997135683125261/FGbFDvDfZIF8dcXt12VOWjArgZnxs14TKodIJu5sBCGzqJJlDLc3K0XlrUcwnaE83AWO"
+//var r2r bladeewebhook = "https://discord.com/api/webhooks/855118313281159179/_5Knr86_tv6QbdsZ4FJ2LbRoVN-XipjphYGv1l4iIWbSJgct4eBHwppfL0m5B9SvckBm"
  
 func main() {
 	var website string
@@ -24,7 +22,9 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	
 	fmt.Printf("Read file. Ready to start monitoring for %s\n", string(dat)) */
+	
 	fmt.Println("Welcome, what site do you want to begin monitoring for. Example:")
 	fmt.Println("https://kith.com/")
 	fmt.Scanln(&website)
@@ -32,11 +32,10 @@ func main() {
 	fmt.Scanln(&input)
 
 	if input == "1" {
-		a := monitor.Scraper{BaseURL: website, Webhook: personalwebhook,}
+		proxies, _ := taskengine.ReadProxies(true)
+		a := monitor.Scraper{BaseURL: website, Webhook: r2rlfwebhook, Proxies: proxies}
+		a.Client, _ = a.NewClient()
 		a.Monitor()
 	}
 	
-
-	
-	//postlocalhost()
 }
